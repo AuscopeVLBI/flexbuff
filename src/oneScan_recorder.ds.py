1fc5ce4fb051ba586ea1f7aef90cf71014131d41
Guifre Molera <gofrito@users.noreply.github.com>
2019-12-20 14:51:22 +1100

    added mode 0 to not care about datastreams confi 1 do the config here

--- src/oneScan_recorder.ds.py
+++ src/oneScan_recorder.ds.py
@@ -6,6 +6,8 @@ Created on Tue Mar 12 16:37:06 2019
 @author: gmolera
 
 Usage: ./oneScan_recorder.py <filename> <scan_time> <number_chans_per_IF> <bandwidth>
+Using the new style of data recording.
+In this case all the streams are sent via the same network port.
 """
 
 import socket, time
@@ -24,7 +26,7 @@ parser.add_argument('scantime',  help='Length of the scan')
 parser.add_argument('nrchannel', help='Number of channels recording per IF')
 parser.add_argument('bandwidth', help='Bandwidth of the IF channel', default=32)
 parser.add_argument('configFile', nargs='?', help='Set the config file', default='/opt/configStation.ini')
-parser.add_argument('recordingMode', nargs='?', help='Specify recording mode 0=old 1=new', default=0)
+parser.add_argument('recordingMode', nargs='?', help='Mode 0: datastream config made from FS 1: datastream config is needed', default=0)
 parser.add_argument('-v', '--version', action='version',version='%(prog)s 1.0')
 
 args       = parser.parse_args()
@@ -53,11 +55,6 @@ try:
 except:
     raise RuntimeError('Failed to connect to {0}'.format(conn_fbff))
 
-'''
-Using the new style of data recording.
-In this case all the streams are sent via the same network port.
-'''
-
 # Send commands and messages via socket communication (encode and decode)
 def fbComms(s,command):
     s.send(command.encode())
@@ -75,10 +72,13 @@ def configure(s, thread, nthreads):
         fbComms(s,'net_port=46227')
 
 
-'1. Having the vdif_frame_ch, we can determine which datastreams should be active '
-#for board in range(6):
-#    if activeIF[board] == '1':
-#        fbComms(sf,'datastream = add : {}.vdif : 192.168.1.{}/{}.*'.format(ifboards[board],ip_orig[board],station))
+if mode == 1:
+    '1. Having the vdif_frame_ch, we can determine which datastreams should be active '
+    for board in range(6):
+        if activeIF[board] == '1':
+            fbComms(sf,'datastream = add : {}.vdif : 192.168.1.{}/{}.*'.format(ifboards[board],ip_orig[board],station))
+else:
+    '2. We basically do nothing, we expect that the Field System has already set the datastreams in the flexbuff'
 
 '''2. We concatenate everything into the same file as they all use same port and different thread IDs
 I don't like to create a file with termination abcd that depend on the ifs, but it's one way.
@@ -93,29 +93,35 @@ THIS PART NEEDS TBT
 fbComms(sf,'datastream?')
 fbComms(sf,'record?')
 fbComms(sf,'evlbi?')
+
+# Start the recording here
 fbComms(sf,'record=on: {}_{}'.format(outputFn,station))
 
-# Make a While loop to control the recording with sleep(5)
+# Make a While loop to control the recording with sleep(1)
 n=0
 while n < length:
-    print('Step: {}\n'.format(str(n*5)))
+    len_per = int(length/10)
+    if n%len_per == 0:
+        print('Recording is being done: {} %\n'.format(n/len_per*10))
     print(fbComms(sf,'evlbi?;tstat?'))
     n = n + 1
     time.sleep(1)
 
+# Stop the recording
 fbComms(sf,'record=off')
 
-'We do some clean up before shutting the door'
-# Clean the datastreams
-sf.send('datastream?'.encode())
-elem_ds = sf.recv(1024).decode().split(':')
+'We do some clean up before shutting the door - only if the datastreams are created here'
+if mode == 1:
+    sf.send('datastream?'.encode())
+    elem_ds = sf.recv(1024).decode().split(':')
 
-for ds in range(int(elem_ds[1])):
-   if ds+1 ==  elem_ds[1]:
-       # If it is the last datastream then
-       fbComms(sf,'datastream = remove :{}'.format(elem_ds[ds+2][0:-2]))
-   else:
-       # If there are more than one datastream
-       fbComms(sf,'datastream = remove :{}'.format(elem_ds[ds+2]))
+    for ds in range(int(elem_ds[1])):
+        if ds+1 ==  elem_ds[1]:
+            # If it is the last datastream then
+            fbComms(sf,'datastream = remove :{}'.format(elem_ds[ds+2][0:-2]))
+        else:
+            # If there are more than one datastream
+            fbComms(sf,'datastream = remove :{}'.format(elem_ds[ds+2]))
 
+# Closing the communication with the Flexbuff
 sf.close()
