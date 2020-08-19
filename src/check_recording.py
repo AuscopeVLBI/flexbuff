f688676675c77081da42bc89d4007a636d0c77b5
AuscopeVLBI <observer@flexbuffke.phys.utas.edu.au>
2020-08-19 07:35:58 +0000

    fairly tested version in Ke

--- src/check_recording.py
+++ src/check_recording.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Mar 12 16:37:06 2019
 
-@author: gmolera
+@author: gofrito
 
 Usage: ./check_recording.py <filename> nchannels bandwidth channel [zoom] min max frequencies
 
@@ -18,23 +18,26 @@ parser.add_argument('filename',   help='Full name of the recorded file')
 parser.add_argument('nchannels',  help='Number of channels per IF')
 parser.add_argument('bandwidth',  help='Bandwidth of the channel')
 parser.add_argument('channel',    help='Select channel to display')
-parser.add_argument('--zoom', '-z', help='specify a zoom window around the desired line', aaction="store_true", default=False)
-parser.add_argument('frequencies', help='Zoom interval')
+parser.add_argument('-zf',dest='zoomfreq',nargs='+',type=float,metavar='N')
 
 args = parser.parse_args()
-inFn = args.filename
-nch  = args.nchannels
-bw   = args.bandwidth
-ch   = args.channel
-zoom = args.zoom
-
-if zoom==True:
-    minfrq = args.frequencies[0]
-    maxfrq = args.frequencies[1]
+inFn: str = args.filename
+nch: str  = args.nchannels
+bw: str   = args.bandwidth
+ch: str   = args.channel
 
 # Unless we do a very short recording we are probably happy to have 5 second integration
-fftpoints = 32e3
-inttime   = 5
+fftpoints: int = 320e3
+inttime: int = 10
+
+# frequency resolution
+df: float = 2*int(bw)*1e6/fftpoints
+
+if args.zoomfreq:
+    fmin: float = args.zoomfreq[0]*1e6
+    fmax: float = args.zoomfreq[1]*1e6
+    bmin: int = int(fmin/df)
+    bmax: int = int(fmax/df)
 
 'Mount the file recording into Flexbuff style disks'
 # Check if something is mounted - Not even check umount
@@ -49,16 +52,18 @@ p = subprocess.Popen(['cp','/home/observer/AuscopeVLBI/flexbuff/config/inifile.i
 
 time.sleep(0.5)
 
-sourceFormat = 'VDIF_8000-{}-{}-2'.format(4*int(bw)*int(nch),nch)
-
+# Open inifile and replace those settings that are observation dependent
 s = open('inifile.ini').read()
-s = s.replace('VDIF_8000-0000-00-00',sourceFormat)
-s = s.replace('SourceChannels    = 0','SourceChannels    = {}'.format(nch))
-s = s.replace('BandwidthHz       = 0','BandwidthHz       = {}'.format(bw))
-s = s.replace('UseFile1Channel   = 0','UseFile1Channel   = {}'.format(ch))
+
+s = s.replace('SourceChannels    = 0',f'SourceChannels = {nch}')
+s = s.replace('BandwidthHz       = 0',f'BandwidthHz = {bw}')
+s = s.replace('UseFile1Channel   = 0',f'UseFile1Channel = {ch}')
+s = s.replace('FFTpoints = 0',f'FFTpoints = {fftpoints}')
+s = s.replace('FFTIntegrationTimeSec = 0',f'FFTIntegrationTimeSec = {inttime}')
 
 f = open('inifile.tmp.ini','w')
 f.write(s)
+
 f.close()
 
 # Then modify the parameters related to the observation
@@ -69,10 +74,10 @@ spec_filename = 'single_channel_swspec.bin'
 
 # visualize the spectra
 
-fsize = os.path.getsize(spec_filename)
-Nspec = np.int(np.floor(0.5*fsize/(fftpoints + 1)))
-Nfft  = np.int(fftpoints/2+1)
-Sps    = np.zeros((Nspec,Nfft))
+fsize: float = os.path.getsize(spec_filename)
+Nspec: int = np.int(np.floor(0.5*fsize/(fftpoints + 1)))
+Nfft: int  = np.int(fftpoints/2+1)
+Sps = np.zeros((Nspec,Nfft), dtype=float)
 
 fd = open(spec_filename,'rb')
 
@@ -82,26 +87,27 @@ for ip in np.arange(Nspec):
 
 Aspec = np.sum(Sps,axis=0)/Nspec
 
-df = 2*np.int(bw)/np.int(fftpoints)
 jf = np.arange(Nfft)
 ff = df*jf
 
 # Make a plot
-if zoom == False:
+if args.zoomfreq:
+    plt.plot(ff[bmin:bmax],np.log10(Aspec[bmin:bmax]))
+    plt.ylabel('Spectrum')
+    plt.xlabel('Freq [Hz]')
+    plt.title('Spectral zoom')
+    plt.show()
+else:
     plt.plot(ff,np.log10(Aspec))
     plt.ylabel('Spectrum')
     plt.xlabel('Freq [Hz]')
     plt.title('Average spectra')
     plt.show()
-else: #Zoom == True:
-    plt.plot(ff[minfrq:maxfrq],np.log10(Aspec[minfrq:maxfrq]))
-    plt.ylabel('Spectrum')
-    plt.xlabel('Freq [Hz]')
-    plt.title('Spectral zoom')
-    plt.show()
 
 # Let's clean our mess
-subprocess.call(['rm','inifile.ini'])
-subprocess.call(['rm','inifile.tmp.ini'])
-subprocess.call(['rm','single_channel*'])
-subprocess.call(['fusermount','-u','~/vbs_tmp'])
+subprocess.call(['rm','./inifile.ini'])
+subprocess.call(['rm','./inifile.tmp.ini'])
+subprocess.call(['rm','./single_channel_swspec.bin'])
+subprocess.call(['rm','./single_channel_runlog.txt'])
+subprocess.call(['rm','./single_channel_starttiming.txt'])
+subprocess.call(['fusermount','-u','/home/observer/vbs_tmp/'])
