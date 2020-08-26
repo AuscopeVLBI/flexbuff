300ace4b2890eee4e19d67fff36429bc693e8eb0
AuscopeVLBI <observer@flexbuffke.phys.utas.edu.au>
2020-08-26 23:16:54 +0000

    update python3 activation

--- src/activate_python3.sh
+++ src/activate_python3.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 echo 'Activating Python 3.8.5 environment'
+# source bashrc for crontab to transition from shell 2 bash
+source ~/.bashrc
+
+# overwrite pythonpath set by DiFX and set python3 env
 export PYTHONPATH=/home/observer/.pyenv/shims/python
 pyenv shell 3.8.5
